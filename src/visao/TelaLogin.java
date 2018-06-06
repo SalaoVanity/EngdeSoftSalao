e820e7bbed03a2fa8305545b85e962bf81c612f4
ThaisGabrielle <salaovanity2018@gmail.com>
2018-06-06 08:18:33 -0300

    Tela de Login - Thaís

---
+++ src/visao/TelaLogin.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visao;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modeloConexao.ConexaoBD;
+
+/**
+ *
+ * @author Rayssa
+ */
+public class TelaLogin extends javax.swing.JFrame {
+    
+    ConexaoBD con = new ConexaoBD();
+
+    public TelaLogin() {
+        initComponents();
+        this.getRootPane().setDefaultButton(btnLogar);
+        
+                // coloca uma figura na barra de título da janela
+        URL url = this.getClass().getResource("/Imagens/salao.png");
+        Image imagemTitulo = Toolkit.getDefaultToolkit().getImage(url);
+        this.setIconImage(imagemTitulo);
+        
+        con.conexao();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnSair = new javax.swing.JButton();
+        btnLogar = new javax.swing.JButton();
+        txtSenha = new javax.swing.JPasswordField();
+        txtUsuario = new javax.swing.JTextField();
+        lblFundoLogin = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        getContentPane().setLayout(null);
+
+        btnSair.setText("Sair");
+        btnSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSairActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnSair);
+        btnSair.setBounds(720, 410, 80, 50);
+
+        btnLogar.setText("Entrar");
+        btnLogar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLogarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnLogar);
+        btnLogar.setBounds(580, 410, 80, 50);
+        getContentPane().add(txtSenha);
+        txtSenha.setBounds(590, 330, 210, 30);
+        getContentPane().add(txtUsuario);
+        txtUsuario.setBounds(590, 230, 210, 30);
+
+        lblFundoLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/Slogan Muito Preto.png"))); // NOI18N
+        getContentPane().add(lblFundoLogin);
+        lblFundoLogin.setBounds(0, 0, 946, 590);
+
+        setSize(new java.awt.Dimension(955, 635));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSairActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_btnSairActionPerformed
+
+    private void btnLogarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLogarActionPerformed
+        try {
+            con.executaSql("select * from usuarios where nome_usuario='"+ txtUsuario.getText() +"'");
+            con.rs.first();
+            if(con.rs.getString("senha_usuario").equals(txtSenha.getText())){
+                TelaPrincipal tela = new TelaPrincipal(txtUsuario.getText());
+                tela.setVisible(true);
+                dispose();
+            } else{
+                JOptionPane.showMessageDialog(rootPane, "Senha inválida!");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(rootPane, "Usuário inválido!");
+            
+        }
+    }//GEN-LAST:event_btnLogarActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaLogin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnLogar;
+    private javax.swing.JButton btnSair;
+    private javax.swing.JLabel lblFundoLogin;
+    private javax.swing.JPasswordField txtSenha;
+    private javax.swing.JTextField txtUsuario;
+    // End of variables declaration//GEN-END:variables
+}
